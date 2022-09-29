289ab80930dc7eef64502f01e91264ce0e1ccae3
eren-olympic <eren.olympic.14@gmail.com>
2022-09-30 02:36:33 +0800

    Initial commit

---
+++ screens/Details.js
@@ -0,0 +1,20 @@
+import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native';
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from '../constants';
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from '../components';
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
+  console.log(`
+    -----------------------------
+    ${data}
+    -----------------------------
+    `
+  )
+
+  return (
+    <Text>Details</Text>
+  )
+}
+
+export default Details
